d8c3ddc8e84b6d513592fc8fff1c473a81266642
Vasilii Ivanov <thebigglu@gmail.com>
2023-06-16 16:30:35 +0200

    reformat emacs config file

--- emacs/init.el
+++ emacs/init.el
@@ -1,3 +1,18 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                                                                          ;;
+;;                          Emacs config file                               ;;
+;;                                                                          ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Name and email                                                           ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq user-full-name    "Vasilii Ivanov")
+(setq user-mail-address "thebigglu@gmail.com")
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Package management                                                       ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (require 'package)
 (add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
@@ -10,24 +25,83 @@
   (setq use-package-always-ensure t
         use-package-expand-minimally t))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; UI                                                                       ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(tooltip-mode      -1)
+(menu-bar-mode     -1)
+(tool-bar-mode     -1)
+(scroll-bar-mode   -1)
+(blink-cursor-mode -1)
+(set-default 'truncate-lines t)
+(setq use-dialog-box nil)
+(setq ring-bell-function 'ignore)
+(setq inhibit-splash-screen t)
+(setq inhibit-startup-message t)
+(setq frame-title-format "Emacs %b")
+
 (use-package tron-legacy-theme
   :config
   (setq tron-legacy-theme-vivid-cursor t)
   (load-theme 'tron-legacy t))
 
-(use-package tree-sitter
-  :config
-  (global-tree-sitter-mode)
-  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+(add-to-list 'default-frame-alist '(font . "Hack 13"))
 
-(use-package tree-sitter-langs)
+(setq isearch-lazy-count t)
 
-(use-package eglot
-  :init
-  (setq eldoc-echo-area-use-multiline-p nil)
-  (setq eglot-ignored-server-capabilities '(:hoverProvider :documentHighlightProvider))
-  :hook ((js-mode typescript-mode) . eglot-ensure))
+(setq column-number-mode t)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Global shortcuts                                                         ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(global-set-key [remap list-buffers] 'ibuffer)
+(global-set-key (kbd "M-o") 'other-window)
+
+(global-set-key (kbd "M-i") 'imenu)
+
+;; disable js-find-symbol because the eglot
+(global-set-key [remap js-find-symbol] 'xref-find-definitions)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Editing                                                                  ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(setq tab-width 2)
+(setq-default indent-tabs-mode nil)
+(setq-default js-indent-level tab-width)
+(setq-default electric-indent-inhibit t)
+(setq backward-delete-char-untabify-method 'hungry)
+
+(setq auto-save-default nil)
+(setq make-backup-files nil)
+(setq auto-save-list-file-name nil)
+(setq create-lockfiles nil)
+
+(use-package expand-region
+  :bind ("C-=" . er/expand-region))
+
+(setq require-final-newline t)
+(add-hook 'before-save-hook 'delete-trailing-whitespace)
+
+;; new line above/below of the cursor
+(global-set-key (kbd "<C-return>") (lambda ()
+  (interactive)
+  (end-of-line)
+  (newline-and-indent)))
+(global-set-key (kbd "<S-return>") (lambda ()
+  (interactive)
+  (previous-line)
+  (end-of-line)
+  (newline-and-indent)))
+
+(set-default-coding-systems 'utf-8)
+
+(delete-selection-mode t)
+
+(electric-pair-mode t)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Search                                                                   ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package helm
   :init (helm-mode t)
   :demand
@@ -36,19 +110,9 @@
   ("C-x r b" . helm-filtered-bookmarks)
   ("C-x C-f" . helm-find-files))
 
-(defun use-eslint-from-node-modules ()
-  (let* ((root (locate-dominating-file (or (buffer-file-name) default-directory) "node_modules"))
-    (eslint (and root (expand-file-name "node_modules/.bin/eslint" root))))
-    (when (and eslint (file-executable-p eslint))
-      (setq-local flycheck-javascript-eslint-executable eslint))))
-
-(use-package flycheck
-  :init (global-flycheck-mode)
-  :hook (flycheck-mode . use-eslint-from-node-modules))
-
-(use-package expand-region
-  :bind ("C-=" . er/expand-region))
-
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Terminal                                                                 ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package vterm
   :init
   (setq vterm-always-compile-module t)
@@ -65,81 +129,38 @@
   ("C-c i d" . multi-vterm-dedicated-toggle)
   ("C-c i i" . multi-vterm-project))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Programming                                                              ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(use-package eglot
+  :init
+  (setq eldoc-echo-area-use-multiline-p nil)
+  (setq eglot-ignored-server-capabilities '(:hoverProvider :documentHighlightProvider))
+  :hook ((js-mode typescript-mode) . eglot-ensure))
+
+(use-package tree-sitter
+  :config
+  (global-tree-sitter-mode)
+  (add-hook 'tree-sitter-after-on-hook #'tree-sitter-hl-mode))
+
+(use-package tree-sitter-langs)
+
+(defun use-eslint-from-node-modules ()
+  (let* ((root (locate-dominating-file (or (buffer-file-name) default-directory) "node_modules"))
+    (eslint (and root (expand-file-name "node_modules/.bin/eslint" root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
+
+(use-package flycheck
+  :init (global-flycheck-mode)
+  :hook (flycheck-mode . use-eslint-from-node-modules))
+
 (use-package typescript-mode)
 (use-package yaml-mode)
 (use-package json-mode)
 (use-package dockerfile-mode)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; VCS                                                                      ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (use-package magit)
-
-;; credentials
-(setq user-full-name    "Vasilii Ivanov")
-(setq user-mail-address "thebigglu@gmail.com")
-
-;; UI
-(tooltip-mode      -1)
-(menu-bar-mode     -1)
-(tool-bar-mode     -1)
-(scroll-bar-mode   -1)
-(blink-cursor-mode -1)
-(set-default 'truncate-lines t)
-(setq use-dialog-box nil)
-(setq ring-bell-function 'ignore)
-(setq inhibit-splash-screen t)
-(setq inhibit-startup-message t)
-(setq frame-title-format "Emacs %b")
-
-;; indentation
-(setq tab-width 2)
-(setq-default indent-tabs-mode nil)
-(setq-default js-indent-level tab-width)
-(setq-default electric-indent-inhibit t)
-(setq backward-delete-char-untabify-method 'hungry)
-
-;; face
-(add-to-list 'default-frame-alist '(font . "Hack 13"))
-
-;; no backups and lock files
-(setq auto-save-default nil)
-(setq make-backup-files nil)
-(setq auto-save-list-file-name nil)
-(setq create-lockfiles nil)
-
-;; new line/spaces
-(setq require-final-newline t)
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-;; new line above/below of the cursor
-(global-set-key (kbd "<C-return>") (lambda ()
-  (interactive)
-  (end-of-line)
-  (newline-and-indent)))
-(global-set-key (kbd "<S-return>") (lambda ()
-  (interactive)
-  (previous-line)
-  (end-of-line)
-  (newline-and-indent)))
-
-;; encoding
-(set-default-coding-systems 'utf-8)
-
-;; switch buffer settings
-(global-set-key [remap list-buffers] 'ibuffer)
-(global-set-key (kbd "M-o") 'other-window)
-
-;; delete region when typing
-(delete-selection-mode t)
-
-;; key for imenu
-(global-set-key (kbd "M-i") 'imenu)
-
-;; disable js-find-symbol because the eglot
-(global-set-key [remap js-find-symbol] 'xref-find-definitions)
-
-;; auto pair brackets
-(electric-pair-mode t)
-
-;; enable search count
-(setq isearch-lazy-count t)
-
-;; enable column number
-(setq column-number-mode t)
