bb78d68cf6d8754696ecac14071b250fcc5f33f7
Vasilii Ivanov <thebigglu@gmail.com>
2023-08-19 21:33:59 +0200

    nvim init config

---
+++ nvim/init.lua
@@ -0,0 +1,115 @@
+vim.g.mapleader = " "
+vim.opt.clipboard = "unnamedplus"
+vim.wo.number = true
+vim.wo.relativenumber = true
+vim.wo.linebreak = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.smartindent = true
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  "folke/tokyonight.nvim",
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    }
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+      "linrongbin16/lsp-progress.nvim",
+    }
+  },
+  {
+    "akinsho/bufferline.nvim",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons"
+    }
+  },
+  {
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim"
+    }
+  },
+  "nvim-treesitter/nvim-treesitter",
+  "windwp/nvim-autopairs",
+  "terrortylor/nvim-comment",
+  "lewis6991/gitsigns.nvim",
+})
+
+vim.cmd.colorscheme("tokyonight-night")
+require("neo-tree").setup()
+require("lualine").setup()
+require("nvim-treesitter.configs").setup({
+  ensure_installed = { "javascript", "typescript", "lua" },
+  sync_install = false,
+  auto_install = true,
+  highlight = {
+    enable = true
+  }
+})
+require("nvim-autopairs").setup()
+require("nvim_comment").setup({
+  line_mapping = "<leader>cl",
+  operator_mapping = "<leader>c"
+})
+require("gitsigns").setup()
+
+
+vim.keymap.set('n', '<leader>E', ':Neotree float reveal<CR>')
+vim.keymap.set('n', '<leader>e', ':Neotree left reveal<CR>')
+vim.keymap.set('n', '<leader>o', ':Neotree float git_status<CR>')
+
+vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
+vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
+vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
+vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+vim.keymap.set('n', '<leader>/', ':CommentToggle<CR>')
+
+vim.keymap.set('n', '<leader>w', ':w<CR>')
+vim.keymap.set('n', '<leader>x', ':BufferLinePickClose<CR>')
+vim.keymap.set('n', '<leader>X', ':BufferLineCloseRight<CR>')
+vim.keymap.set('n', '<leader>s', ':BufferLineSortByTabs<CR>')
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
+
+vim.keymap.set('n', '<Tab>', ':BufferLineCycleNext<CR>')
+vim.keymap.set('n', '<s-Tab>', ':BufferLineCyclePrev<CR>')
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fw', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+-- vim.keymap.set('n', '<Tab>', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
+vim.keymap.set('n', '<leader>gc', builtin.git_commits, {})
+vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
+vim.keymap.set('n', '<leader>ls', builtin.lsp_document_symbols, {})
+vim.keymap.set('n', 'gr', builtin.lsp_references, {noremap = true, silent = true})
+vim.keymap.set('n', 'gd', builtin.lsp_definitions, {noremap = true, silent = true})
